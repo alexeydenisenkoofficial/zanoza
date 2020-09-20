cb1cb64946494b647396ffdef282cd7900a62b12
alexeydenisenkoofficial <71227293+alexeydenisenkoofficial@users.noreply.github.com>
2020-09-20 16:22:02 +0300

    Create Main.java

---
+++ src/The_second/N1/Main.java
@@ -0,0 +1,10 @@
+package The_second.N1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Shape shape = new Shape("red", 10);
+        System.out.println(shape);
+    }
+
+}
