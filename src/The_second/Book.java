147ec2919e4fc9c69f97dd54af39ce337560a55b
alexeydenisenkoofficial <71227293+alexeydenisenkoofficial@users.noreply.github.com>
2020-09-20 16:02:53 +0300

    Create Book.java

---
+++ src/The_second/Book.java
@@ -0,0 +1,13 @@
+package The_Second;
+
+public class Book {
+    private int year = 2000;
+    private String name = "1984";
+
+    public Book(){
+    }
+
+    public static void main(String[] args) {
+        Book b001 = new Book();
+    }
+}
