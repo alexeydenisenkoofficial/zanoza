38bfa074f89828a47d1021d9befdd7ac591f78e5
alexeydenisenkoofficial <71227293+alexeydenisenkoofficial@users.noreply.github.com>
2020-09-20 15:58:18 +0300

    Create Myach.java

---
+++ src/The_second/Myach.java
@@ -0,0 +1,15 @@
+package The_Second;
+
+public class Myach {
+
+    private String material;
+    private String color;
+    private int size;
+
+    public Myach(){
+    }
+
+    public static void main(String[] args) {
+        Myach ball = new Myach();
+    }
+}
