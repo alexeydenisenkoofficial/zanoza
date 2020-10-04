741941f63c0b0f2e0b6b802985ae78e1206c6da4
alexeydenisenkoofficial <71227293+alexeydenisenkoofficial@users.noreply.github.com>
2020-10-04 20:49:43 +0300

    Create TestBook.java

---
+++ src/3/TestBook.java
@@ -0,0 +1,8 @@
+package 3;
+
+public class TestBook {
+    public static void main(String[] args){
+        Book mybook = new Book("Мечтать не вредно", 2032, "Алексей", 333, 500);
+        System.out.println(book);
+    }
+}
