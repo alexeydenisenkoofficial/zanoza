ba28a1171d9f2f85a8f492b6420488cb84966ce5
alexeydenisenkoofficial <71227293+alexeydenisenkoofficial@users.noreply.github.com>
2020-10-10 15:08:54 +0300

    Delete number_one_two.java

--- src/number_one_two.java
+++
@@ -1,9 +0,0 @@
-public class number_one_two {
-    public static void main(String[] args)
-    {
-        for (int i=0; i<args.length; i++)
-        {
-            System.out.println("Аргумент "+args[i]);
-        }
-    }
-}
